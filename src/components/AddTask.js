899d3091a17d4aeb7357f2d6eb22daa2eb0cd91f
Silverstar323 <silverstars323@gmail.com>
2024-08-07 07:31:33 -0700

    fourth

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,20 +4,20 @@ const AddTask = ({ taskList, setTaskList, task, setTask }) => {
     const data = new Date();
     if (task.id) {
       setTaskList(taskList.map(todo => (
-        todo.id === task.id ? {id: task.id, name: task.name, time: `${data.toLocaleTimeString()} ${data.toLocaleDateString()}`} : todo
+        todo.id === task.id ? { id: task.id, name: task.name, time: `${data.toLocaleTimeString()} ${data.toLocaleDateString()}` } : todo
       )));
       setTask("");
     } else {
       const newTask = { id: data.getTime(), name: event.target.task.value, time: `${data.toLocaleTimeString()} ${data.toLocaleDateString()}` };
       setTaskList([...taskList, newTask]);
-      event.target.task.value = "";
     }
+    event.target.task.value = "";
   }
 
   return (
     <section className="addTask">
       <form onSubmit={handleSubmit}>
-        <input type="text" name="task" defaultValue={task.name} autoComplete="off" placeholder="add task" maxLength="25" onChange={e => setTask({...task, name: e.target.value})} />
+        <input type="text" name="task" defaultValue={task.name} autoComplete="off" placeholder="add task" maxLength="25" onChange={e => setTask({ ...task, name: e.target.value })} />
         <button type="submit">{Boolean(task.name) ? "Upgrade" : "Add"}</button>
       </form>
     </section>
